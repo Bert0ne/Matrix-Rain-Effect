28609f4b617a5150761aa1582f30f96ebe1a5eb4
Robert <wybawiciel@gmail.com>
2022-03-03 07:46:48 +0100

    project ready

--- js.js
+++ js.js
@@ -1,43 +1,82 @@
-
-  
-const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
+canvas = document.getElementById('Matrix');
+context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
+window.addEventListener('resize', function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+})
+class Matrix {
 
-const alphabet = katakana + latin + nums;
-
-const fontSize = 16;
-const columns = canvas.width/fontSize;
-
-const rainDrops = [];
-
-for( let x = 0; x < columns; x++ ) {
-	rainDrops[x] = '0';
-}
-
-const draw = () => {
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
+	 katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+	 latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	 nums = '0123456789';
+	 alphabet = this.katakana + this.latin + this.nums;
+	 fontSize = 16;
+	 columns = canvas.width/this.fontSize;
+	 rainDrops = [];
 	
-	context.fillStyle = '#0F0';
-	context.font = fontSize + 'px monospace';
+	 constructor(rainSpeed) {
+		this.createColumns()
+		this.rainInterval(rainSpeed);
+		this.resizeListener();
+	 }
 
-	for(let i = 0; i < rainDrops.length; i++)
-	{
-		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+	 addArrIndexLoop(number,operation) {
+		 for(let i = 0; i < number; i++) {
+			 operation == 'push' ? this.rainDrops.push(0) : this.rainDrops.pop()
+		 }
+	 } 
+
+	 resizeListener() {
+		 window.addEventListener('resize',() => {
+			let rainDropsL = this.rainDrops.length;
+			let columnsLength = Math.floor(window.innerWidth / this.fontSize)
+			let currCol;
+
+			if(columnsLength > rainDropsL){
+				currCol = columnsLength - rainDropsL
+				this.addArrIndexLoop(currCol, 'push')			
+			}
+			if(columnsLength < rainDropsL) {
+				currCol = rainDropsL - columnsLength;
+				this.addArrIndexLoop(currCol, '')			
+			}
+		 })
+	 }
+
+	 rainInterval(rainSpeed) {
+		 setInterval(() => {
+			 this.drawRainDrop()
+		 }, rainSpeed);
+	 }
+
+	 createColumns(){
+		 for(let i = 0; i < this.columns; i++) {
+			 this.rainDrops[i] = '0'
+		 }
+	 }
+
+	 drawRainDrop = () => {
+		context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+		context.fillRect(0, 0, canvas.width, canvas.height);
 		
-		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
-			rainDrops[i] = 0;
-        }
-		rainDrops[i]++;
-	}
-};
-
-setInterval(draw, 20);
+		context.fillStyle = '#0F0';
+		context.font = this.fontSize + 'px monospace';
+	
+		for(let i = 0; i < this.rainDrops.length; i++)
+		{
+			const text = this.alphabet.charAt(Math.floor(Math.random() * this.alphabet.length));
+			context.fillText(text, i * this.fontSize, this.rainDrops[i] * this.fontSize);
+			
+			if(this.rainDrops[i] * this.fontSize > canvas.height && Math.random() > 0.975){
+				this.rainDrops[i] = 0;
+			}
+			this.rainDrops[i]++;
+		}
+	};
+}
+  
+new Matrix(20);
